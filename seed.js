bdf5cfb48881fcabd1261af94b928ba0dad6c30c
James Reynolds <jameshreynolds@gmail.com>
2018-03-15 15:31:09 -0700

    getting set up for multi processing insert

--- seed.js
+++ seed.js
@@ -1,4 +1,52 @@
-var dataObj = require('./single_restaurant_data.js');
-var database = require('./db/models/restaurant.js');
+const MongoClient = require('mongodb').MongoClient;
+const _ = require('ramda');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-console.log(dataObj.data)
+var time = new Date().getTime();
+
+var obj = {
+    test1: 'kjwbfjblbcjbckjbckjcbkjsc',
+    test2: 'jcbkjbskcbskdjcbkjsdbcksjbskjbsdkjcbs',
+    test3: 'jcnkjsbcnkjsdbcksdbcskcbsddsckb'
+}
+
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} finished`)
+    });
+} else {
+    seedDB();
+    console.log(`Worker ${process.pid} started`)
+}
+
+
+function seedBD() {
+    MongoClient.connect(`mongodb://localhost/`).then((client) => {
+        const db = client.db('recs');
+        const collection = db.connection('testing');
+
+        var count = parseInt(10000000 / numCPUs);
+        const size = 20000;
+
+        async function insertBulk() {
+            var ops = _.range(0, size).map((id) => {
+                return {insertOne: {'document': {...obj, rid: id*Math.random() } }};
+            });
+            await collection.bulkWrite(ops, {ordered: false});
+            count -= size;
+            if (count > 0) {
+                insertBulk();
+            } else {
+                console.log('done in', (new Date().getTime() - time) / 1000, 's <3 <@;) ,(^_^<))  ');
+                client.close();
+                process.exit();
+            }
+        }
+        insertBulk();
+    });
+}
